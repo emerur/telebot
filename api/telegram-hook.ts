99fadf218fb091efed4faf925c9b5d751cab65f5
emerur <juiceless.9@gmail.com>
2025-06-28 18:17:22 +0000

    Update telegram-hook.ts

--- api/telegram-hook.ts
+++ api/telegram-hook.ts
@@ -37,10 +37,8 @@ Ready to browse without borders? Get today's list below
   parse_mode: "Markdown",
   reply_markup: {
     inline_keyboard: [
-      [
          [{ text: "Get Today's Socks5", callback_data: "socks_5" }],
         [{ text: "Get Today's Socks4", callback_data: "socks_4" }]
-      ],
     ],
   },
 });
